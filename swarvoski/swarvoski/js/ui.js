f710bb0cecf659fabd9b935a5a1a122df62c87e8
heksamin <happsome@gmail.com>
2021-05-15 22:45:21 +0900

    반응형 사이트 리뉴얼중 업로드

---
+++ swarvoski/swarvoski/js/ui.js
@@ -0,0 +1,30 @@
+$(function() {
+    $('.mo_gnb > ul >li > a').click(function() {
+        $('.mo_sub').stop().slideUp();
+        $('.mo_gnb > ul > li').removeClass('on');
+        $(this).parent().find('.mo_sub').stop().slideToggle(function() {
+            if ($(this).parent().find('.mo_sub').is(':hidden')) {
+                $(this).parent().removeClass('on');
+            } else {
+                $(this).parent().addClass('on');
+            }
+        });
+    });
+    $('.open').click(function() {
+        $('body').css('overflow-y', 'hidden');
+        $('.mo_gnb,.mo_bg').stop().animate({
+            right: '0',
+            opacity: 'show'
+        }, 1000);
+    });
+
+    $('.close, .mo_bg').click(function() {
+        $('body').css('overflow-y', 'auto');
+        $('.mo_sub').stop().slideUp();
+        $('.mo_gnb > ul > li').removeClass('on');
+        $('.mo_gnb,.mo_bg').stop().animate({
+            right: '-100%',
+            opacity: 'hide'
+        }, 1000);
+    });
+});
