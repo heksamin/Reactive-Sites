8e1049d1f5254c3aa0d60a0492ed91fa13c17c79
heksamin <happsome@gmail.com>
2021-05-16 23:48:42 +0900

    메뉴 슬라이드 효과 적용 완료. 제이쿼리 사용

---
+++ swarvoski/js/ui.js
@@ -0,0 +1,38 @@
+$(function() {
+    $(".gnb ul li").mouseover(function(){
+        $(".gnb ul li .depth2").stop().slideDown(300);
+    });
+    $(".gnb ul li").mouseleave(function(){
+        $(".gnb ul li .depth2").stop().slideUp(300);
+    });
+
+
+    $('.mo_gnb > ul >li > a').click(function() {
+        $('.mo_sub').stop().slideUp();
+        $('.mo_gnb > ul > li').removeClass('on');
+        $(this).parent().find('.mo_sub').stop().slideToggle(function() {
+            if ($(this).parent().find('.mo_sub').is(':hidden')) {
+                $(this).parent().removeClass('on');
+            } else {
+                $(this).parent().addClass('on');
+            }
+        });
+    });
+    $('.open').click(function() {
+        $('body').css('overflow-y', 'hidden');
+        $('.mo_gnb,.mo_bg').stop().animate({
+            right: '0',
+            opacity: 'show'
+        }, 1000);
+    });
+
+    $('.close, .mo_bg').click(function() {
+        $('body').css('overflow-y', 'auto');
+        $('.mo_sub').stop().slideUp();
+        $('.mo_gnb > ul > li').removeClass('on');
+        $('.mo_gnb,.mo_bg').stop().animate({
+            right: '-100%',
+            opacity: 'hide'
+        }, 1000);
+    });
+});
