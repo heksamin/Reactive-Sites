6d416ae090a08495a1627384af5bd20f02107322
heksamin <68495717+heksamin@users.noreply.github.com>
2021-07-02 21:25:31 +0900

    Delete js directory

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $(".gnb > ul > li").mouseover(function(){
-        $(".gnb > ul > li > ul").show();
-    });
-    $(".gnb > ul > li").mouseleave(function(){
-        $(".gnb > ul > li > ul").hide();
-    });
-});
